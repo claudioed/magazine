385c3fd42f8283e1175595d4a7dd9fdfb8a48bf9
Claudio Eduardo de Oliveira <claudioeduardo.deoliveira@sonymobile.com>
2015-08-11 12:19:36 -0300

    Available Stock
    Change-Id: I8a29113aaf8f12274200545e936dd4d070c50ff8

--- stock/src/main/java/domain/stock/receiver/RegisterStockByGathering.java
+++ stock/src/main/java/domain/stock/receiver/RegisterStockByGathering.java
@@ -3,28 +3,40 @@ package domain.stock.receiver;
 import domain.event.DomainCollection;
 import domain.event.DomainDb;
 import domain.event.DomainEvent;
-import infra.formatter.DateTimeMongoFormat;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.mongo.MongoClient;
-
-import java.time.LocalDateTime;
+import io.vertx.ext.mongo.UpdateOptions;
 
 /**
  * @author Claudio E. de Oliveira (claudioed.oliveira@gmail.com).
  */
 public class RegisterStockByGathering extends AbstractVerticle {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegisterStockByGathering.class);
+
     @Override
     public void start() throws Exception {
         final MongoClient mongoClient = MongoClient.createShared(vertx,
-                new JsonObject().put(DomainDb.CUSTOMER.db(), DomainDb.CUSTOMER.db()), DomainDb.CUSTOMER.db());
+                new JsonObject().put(DomainDb.CUSTOMER.db(), DomainDb.CUSTOMER.db()),
+                DomainDb.CUSTOMER.db());
         EventBus eb = vertx.eventBus();
-        eb.consumer(DomainEvent.REGISTER_ITEM_BY_GATHERING.event(), message ->
-                mongoClient.insert(DomainCollection.ITEMS.collection(), new JsonObject(message
-                        .body().toString()).put("registeredAt", new JsonObject().put("$date", DateTimeMongoFormat.format(LocalDateTime.now()))), result -> {
-                }));
+        eb.consumer(DomainEvent.REGISTER_ITEM_BY_SALE.event(), message -> {
+            JsonObject saleItem = new JsonObject(message.body().toString());
+            JsonObject query = new JsonObject().put("barcode.plainBarcode", saleItem.getString("plainBarcode"));
+            JsonObject update = new JsonObject().put("$set", new JsonObject().put("available", Boolean.FALSE));
+            UpdateOptions options = new UpdateOptions().setMulti(false);
+            mongoClient.updateWithOptions(DomainCollection.ITEMS.collection(),query,update,options,result ->{
+                if(result.failed()){
+                    LOGGER.error("Error on update stock by gathering !!! Any rows affected !!!");
+                } else {
+                    LOGGER.info("Success on update stock by gathering !!!");
+                }
+            });
+        });
     }
 
 }
--- stock/src/main/java/domain/stock/receiver/RegisterStockBySale.java
+++ stock/src/main/java/domain/stock/receiver/RegisterStockBySale.java
@@ -1,30 +1,43 @@
+
 package domain.stock.receiver;
 
 import domain.event.DomainCollection;
 import domain.event.DomainDb;
 import domain.event.DomainEvent;
-import infra.formatter.DateTimeMongoFormat;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.mongo.MongoClient;
-
-import java.time.LocalDateTime;
+import io.vertx.ext.mongo.UpdateOptions;
 
 /**
  * @author Claudio E. de Oliveira (claudioed.oliveira@gmail.com).
  */
 public class RegisterStockBySale extends AbstractVerticle {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegisterStockBySale.class);
+
     @Override
     public void start() throws Exception {
         final MongoClient mongoClient = MongoClient.createShared(vertx,
-                new JsonObject().put(DomainDb.CUSTOMER.db(), DomainDb.CUSTOMER.db()), DomainDb.CUSTOMER.db());
+                new JsonObject().put(DomainDb.CUSTOMER.db(), DomainDb.CUSTOMER.db()),
+                DomainDb.CUSTOMER.db());
         EventBus eb = vertx.eventBus();
-        eb.consumer(DomainEvent.REGISTER_ITEM_BY_SALE.event(), message ->
-                mongoClient.insert(DomainCollection.ITEMS.collection(), new JsonObject(message
-                        .body().toString()).put("registeredAt", new JsonObject().put("$date", DateTimeMongoFormat.format(LocalDateTime.now()))), result -> {
-                }));
+        eb.consumer(DomainEvent.REGISTER_ITEM_BY_SALE.event(), message -> {
+            JsonObject saleItem = new JsonObject(message.body().toString());
+            JsonObject query = new JsonObject().put("barcode.plainBarcode", saleItem.getString("plainBarcode"));
+            JsonObject update = new JsonObject().put("$set", new JsonObject().put("available", Boolean.FALSE));
+            UpdateOptions options = new UpdateOptions().setMulti(false);
+            mongoClient.updateWithOptions(DomainCollection.ITEMS.collection(),query,update,options,result ->{
+                if(result.failed()){
+                    LOGGER.error("Error on update stock by sale !!! Any rows affected !!!");
+                } else {
+                    LOGGER.info("Success on update stock by sale !!!");
+                }
+            });
+        });
     }
 
 }
